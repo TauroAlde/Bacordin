552bf772e981d3fbe355c63a2f39be632650e873
Amed <amed157@hotmail.com>
2017-11-10 20:05:56 -0500

    add filds to model studen

--- db/migrate/20171111004919_create_students.rb
+++ db/migrate/20171111004919_create_students.rb
@@ -1,7 +1,15 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-
+      t.string :first_name
+      t.string :father_last_name
+      t.string :mother_last_name
+      t.integer :tag_student
+      t.string :institutional_email
+      t.string :personal_email
+      t.string :reference_name
+      t.integer :cell_phone
+      t.string :level_englsih
       t.timestamps null: false
     end
   end
